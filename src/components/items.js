658b0526dfc9fef312b7d1ddd17628a5761bc46e
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 16:29:03 +0530

    Adding items as template to render in place of products

---
+++ src/components/items.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    name: "iPhone 11",
+    brand: "Apple",
+    price: "53,500 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/51kGDXeFZKL._SX466_.jpg",
+  },
+  {
+    name: "iPhone 7",
+    brand: "Apple",
+    price: "27,800 INR",
+    quantity: 20,
+    src: "https://images-na.ssl-images-amazon.com/images/I/41D9NDiSzjL._SY606_.jpg",
+  },
+  {
+    name: "iPhone X",
+    brand: "Apple",
+    price: "64,789 INR",
+    quantity: 5,
+    src: "https://images-na.ssl-images-amazon.com/images/I/411SxsDFpsL._SL1024_.jpg",
+  },
+  {
+    name: "iPhone XS",
+    brand: "Apple",
+    price: "99,900 INR",
+    quantity: 15,
+    src: "https://images-na.ssl-images-amazon.com/images/I/51wU55p7DnL._SL1000_.jpg",
+  },
+];
+
+export default items;
