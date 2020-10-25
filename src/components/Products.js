c04bd97991497c943e4fdea1b040e26495a0534a
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 16:24:51 +0530

    added card rendering for items

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,45 @@
+import React, { useState } from "react";
+import items from "./items";
+
 const Products = () => {
+  const [phones] = useState(items);
+  const renderItems = phones.map((item, index) => {
+    return (
+      <div key={index} className="col mb-3" data-index={index}>
+        <div
+          className="card"
+          data-index={index}
+          style={{ cursor: "pointer" }}
+          title={item.name}
+        >
+          <img
+            src={item.src}
+            alt={item.name}
+            data-index={index}
+            style={{ height: "240px" }}
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{item.brand}</h6>
+            <p className="card-text">
+              {" "}
+              <strong>Price: </strong> {item.price}
+            </p>
+            <p className="card-text">
+              {" "}
+              <strong>Quantity: </strong> {item.quantity}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="Products">
-      <h1>Products</h1>
+      <div className="row row-cols-2 row-cols-sm-3 row-cols-md-6">
+        {renderItems}
+      </div>
     </div>
   );
 };
