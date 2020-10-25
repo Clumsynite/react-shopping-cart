274fd75aa3bac4cbce3ee3a48d2410e210f35dde
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 17:23:04 +0530

    Added more items

--- src/components/items.js
+++ src/components/items.js
@@ -1,5 +1,12 @@
 const items = [
   {
+    name: "iPhone 11 Pro Max",
+    brand: "Apple",
+    price: "1,50,800 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/61tuQdl2yLL._SL1024_.jpg",
+  },
+  {
     name: "iPhone 11",
     brand: "Apple",
     price: "53,500 INR",
@@ -7,11 +14,11 @@ const items = [
     src: "https://images-na.ssl-images-amazon.com/images/I/51kGDXeFZKL._SX466_.jpg",
   },
   {
-    name: "iPhone 7",
+    name: "iPhone XS",
     brand: "Apple",
-    price: "27,800 INR",
-    quantity: 20,
-    src: "https://images-na.ssl-images-amazon.com/images/I/41D9NDiSzjL._SY606_.jpg",
+    price: "99,900 INR",
+    quantity: 15,
+    src: "https://images-na.ssl-images-amazon.com/images/I/51wU55p7DnL._SL1000_.jpg",
   },
   {
     name: "iPhone X",
@@ -21,11 +28,60 @@ const items = [
     src: "https://images-na.ssl-images-amazon.com/images/I/411SxsDFpsL._SL1024_.jpg",
   },
   {
-    name: "iPhone XS",
+    name: "iPhone 7",
     brand: "Apple",
-    price: "99,900 INR",
-    quantity: 15,
-    src: "https://images-na.ssl-images-amazon.com/images/I/51wU55p7DnL._SL1000_.jpg",
+    price: "27,800 INR",
+    quantity: 20,
+    src: "https://images-na.ssl-images-amazon.com/images/I/41D9NDiSzjL._SY606_.jpg",
+  },
+  {
+    name: "iPhone 6s",
+    brand: "Apple",
+    price: "29,900 INR",
+    quantity: 20,
+    src: "https://images-na.ssl-images-amazon.com/images/I/516jNlSnB-L._SL1024_.jpg",
+  },
+  {
+    name: "Galaxy Z Fold2",
+    brand: "Samsung",
+    price: "1,49,990 INR",
+    quantity: 5,
+    src: "https://images-na.ssl-images-amazon.com/images/I/61XzsNK3o6L._SL1500_.jpg",
+  },
+  {
+    name: "Galaxy Note10",
+    brand: "Samsung",
+    price: "73,600 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/710H5rWYI0L._SL1500_.jpg",
+  },
+  {
+    name: "Galaxy Note10 Lite",
+    brand: "Samsung",
+    price: "39,999 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/71nUj6kF%2BML._SL1500_.jpg",
+  },
+  {
+    name: "Galaxy Note20 Ultra",
+    brand: "Samsung",
+    price: "1,04,999 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/816LQ5R1iOL._SL1500_.jpg",
+  },
+  {
+    name: "Galaxy Note20",
+    brand: "Samsung",
+    price: "77,999 INR",
+    quantity: 10,
+    src: "https://images-na.ssl-images-amazon.com/images/I/810PIWlXNvL._SL1500_.jpg",
+  },
+  {
+    name: "OnePlus 8",
+    brand: "OnePlus",
+    price: "44,999 INR",
+    quantity: 18,
+    src: "https://images-na.ssl-images-amazon.com/images/I/71SxKcUeSDL._SL1500_.jpg",
   },
 ];
 
