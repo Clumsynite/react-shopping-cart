33a4a78e9e83c8e7de2db165b1eea64510d891e7
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 15:14:59 +0530

    imported router methods
    added navbar in router

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
+    <div className="Routes">
+      <Router>
+        <Navbar/>
+      </Router>
     </div>
   );
 }
