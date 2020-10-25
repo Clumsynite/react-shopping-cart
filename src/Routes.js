abc2925c13d5c1fb565b53ca880e1d10dfa39e66
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 15:47:06 +0530

    Updated to add Router

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,21 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 export default function App() {
   return (
     <div className="Routes">
       <Router>
-        <Navbar/>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={Products} />
+            <Route exact path="/cart" component={Cart} />
+          </Switch>
+        </div>
       </Router>
     </div>
   );
