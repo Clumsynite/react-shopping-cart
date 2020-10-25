ef5627acd6e69bd9df12f9fc87a4f830eea63a4c
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 15:14:28 +0530

    updated to add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <nav></nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link to="/" className="navbar-brand">
+          Shop
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/products" className="nav-link">
+                  Products
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
