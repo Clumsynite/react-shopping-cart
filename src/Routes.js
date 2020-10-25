5af0fbb2939178ff09579192f80bd1b2e40581e0
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-25 21:59:07 +0530

    added Error display and quantity validation

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import items from "./components/items";
 import Navbar from "./components/Navbar";
+import Error from "./components/Error";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
@@ -15,21 +16,34 @@ import Footer from "./components/Footer";
 export default function App() {
   const [phones] = useState(items);
   const [cart, setcart] = useState([]);
-
-  const addToCart = (e) => {
-    const index = e.target.getAttribute('index')
-    console.log(index)
-  } 
+  const [error, setError] = useState("");
+  useEffect(() => {
+    return setTimeout(() => {
+      setError("");
+    }, 5000);
+  }, [error, setError]);
+  const addToCart = (index, quan) => {
+    let quantity = quan || 0;
+    if (quantity === 0) {
+      setError(`You have to add atleast 1 ${items[index].name}`);
+    } else if (items[index].quantity < quantity) {
+      setError(
+        `Can't add more ${items[index].name} to cart. Max Limit: ${items[index].quantity}`
+      );
+    }
+  };
 
   return (
     <div className="Routes">
       <Router>
         <Navbar />
+
         <div className="container">
+          {error && <Error error={error} />}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/products">
-              <Products phones={phones} cart={cart} addToCart={addToCart}/>
+              <Products phones={phones} cart={cart} addToCart={addToCart} />
             </Route>
             <Route exact path="/cart" component={Cart} />
             <Redirect to="/" />
