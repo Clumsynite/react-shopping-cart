68cdf213fcd9266112a13eab1a095f87dede8894
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-26 17:47:59 +0530

    updated to get cart array

--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,11 @@ import Footer from "./components/Footer";
 
 export default function App() {
   const [phones, setPhones] = useState(items);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const array = phones.filter((phone) => phone.cart > 0);
+    setCart(array);
+  }, [phones, setCart]);
   const [error, setError] = useState("");
   useEffect(() => {
     return setTimeout(() => {
@@ -23,7 +28,7 @@ export default function App() {
   }, [error, setError]);
 
   const addToCart = (index, quan) => {
-    let quantity = quan || 0;
+    let quantity = Number(quan) || 0;
     if (quantity === 0) {
       setError(`You have to add atleast 1 ${items[index].name}`);
     } else if (items[index].quantity < quantity) {
@@ -32,22 +37,22 @@ export default function App() {
       );
     } else {
       const array = phones;
-      const item = array[index]
+      const item = array[index];
       const newQuantity = item.quantity - quantity;
-      item.quantity = newQuantity
-      item.cart = item.cart + quantity
-      setPhones(array)
+      item.quantity = newQuantity;
+      item.cart = item.cart + quantity;
+      setPhones(array);
     }
   };
 
   const cartCount = () => {
-    return phones.filter(phone => phone.cart > 0).length
-  }
+    return phones.filter((phone) => phone.cart > 0).length;
+  };
 
   return (
     <div className="Routes">
       <Router>
-        <Navbar cartCount={cartCount}/>
+        <Navbar cartCount={cartCount} />
 
         <div className="container">
           {error && <Error error={error} />}
